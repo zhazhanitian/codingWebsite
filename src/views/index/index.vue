987b2591c91a923a9f4b23b9541545d0ca6f0b55
Ubuntu <nimbus-user@hzora.we.usenimbus.com>
2022-03-31 02:03:37 +0000

    feat: deal rabish code

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -4,19 +4,12 @@
 </template>
 
 <script lang="ts">
-import { useRouter } from 'vue-router'
-import { defineComponent, onMounted } from 'vue'
+import { defineComponent } from 'vue'
 import NavBar from './components/navBar/index.vue'
 import PageContent from './components/content/index.vue'
 
 export default defineComponent({
   components: { NavBar, PageContent },
-  setup() {
-    const router = useRouter()
-
-    onMounted(() => {
-      console.log('router', router)
-    })
-  }
+  setup() {}
 })
 </script>
